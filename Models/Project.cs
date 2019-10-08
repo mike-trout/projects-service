07c21806bd387fcbfa176bf021f74717626e5212
Mike Trout <mike.trout@gmail.com>
2019-10-08 15:30:59 +0100

    Add [Required] decorator to all fields.

--- Models/Project.cs
+++ Models/Project.cs
@@ -5,8 +5,13 @@ namespace ProjectsService.Models
     public class Project
     {
         [Key]
+        [Required]
         public long Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string Description { get; set; }
     }
 }
