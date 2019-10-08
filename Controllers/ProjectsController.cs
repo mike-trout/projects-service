92cae4372089d0c64c2feff6d533d3348bfb2914
Mike Trout <mike.trout@gmail.com>
2019-10-08 18:18:47 +0100

    More content updates.

--- Controllers/ProjectsController.cs
+++ Controllers/ProjectsController.cs
@@ -24,35 +24,49 @@ namespace ProjectsService.Controllers
                 {
                     Id = 1,
                     Name = "www.miketrout.dev",
-                    Description = 
-                        @"<p>This site is written in plain HTML, CSS and JavaScript, deliberately
-                        avoiding the use of frameworks. It is built into a container image,
-                        using the Nginx Docker image as a base. The running container serves
-                        the site using Nginx. The site is deployed as a Kubernetes deployment on
-                        Google Kubernetes Engine and exposed as a service. A GKE ingress deals
-                        with HTTPS, SSL termination and managed certificates from LetsEncrypt.
-                        The site is versioned on
-                        <a href=""https://github.com/mike-trout/www.miketrout.dev""
-                        target=""_blank"">GitHub</a>.
-                        Pushes to master are built and deployed to GKE by Travis CI.</p>
-                        <p>The Experience, Projects and Skills resources are retrieved from an
-                        API hosted at
-                        <a href=""https://api.miketrout.dev"">api.miketrout.dev</a>. The
-                        loading times are deliberately exagerated to visually emphasise the
-                        fact that they are being retrieved from an API. Ambassador is deployed
-                        to GKE and used as a simple API gateway. Ambassador was chosen because
-                        of its simplicity and because it is Kubernetes native. The /experience,
-                        /projects and /skills paths are routed to the backend experience, projects
-                        and skills microservices. The repository for the API gateway is also on
-                        <a href=""https://github.com/mike-trout/api.miketrout.dev
-                        target=""_blank"">GitHub</a>. This
-                        repository is also automatically deployed by Travis CI.</p>"
+                    Description =
+@"<p>This site is written in plain HTML, CSS and JavaScript, deliberately avoiding the use of
+frameworks. It is built into a container image, using the Nginx Docker image as a base. The
+running container serves the site using Nginx. The site is deployed as a Kubernetes deployment on
+Google Kubernetes Engine and exposed as a service. A GKE Ingress deals with HTTPS, SSL termination
+and managed certificates from LetsEncrypt. The site is versioned on
+<a href=""https://github.com/mike-trout/www.miketrout.dev"">GitHub</a>. Pushes to master are built
+and deployed to GKE by Travis CI.</p>
+
+<p>The Experience, Projects and Skills resources are retrieved from an API hosted at
+<a href=""https://api.miketrout.dev"">https://api.miketrout.dev</a>. The loading times are
+deliberately exaggerated in JavaScript and CSS to visually emphasise the fact that they are being
+retrieved from an API. Ambassador is deployed to GKE and used as a simple API gateway. Ambassador
+was chosen because of its simplicity and because it is Kubernetes native. The
+<a href=""https://api.miketrout.dev/experience"">/experience</a>,
+<a href=""https://api.miketrout.dev/projects"">/projects</a> and
+<a href=""https://api.miketrout.dev/skills"">/skills</a> paths are routed to the backend
+experience, projects and skills microservices. The repository for the API gateway is also on
+<a href=""https://github.com/mike-trout/api.miketrout.dev"">GitHub</a> and also automatically
+deployed by Travis CI.</p>
+
+<p>The
+<a href=""https://github.com/mike-trout/experience-service"">experience microservice</a>
+is written in JavaScript and runs on Node.js. The
+<a href=""https://github.com/mike-trout/projects-service"">projects microservice</a>
+that returns this text is written in C# and uses the ASP.NET Core framework. The
+<a href=""https://github.com/mike-trout/skills-service"">skills microservice</a>
+is written in Go and runs as a single binary. All the services are containerised and deployed as
+Kubernetes deployments to GKE. NodePort services exposes them to the Ambassador resource mappings.
+Travis CI pipelines automate the build, test and deploy processes.</p>
+
+<p>Of course, the highly available, horizontally scalable architecture is massively overengineered
+for the amount of traffic that this site receives. But that's kind of the point!</p>"
                 });
                 _context.Projects.Add(new Project
                 {
                     Id = 2,
                     Name = "Software AG Natural Microservice PoC",
-                    Description = "A proof-of-concept for a Software AG Natural and Adabas microservice."
+                    Description =
+@"<p>This project is a proof-of-concept to demonstrate that business logic tied up in legacy Software
+AG Natural code can be exposed as containerised microservices deployed to Kubernetes. It consists
+of two parts: the backend microservice that runs Natural code accessing a containerised Software AG
+Adabas database; and a frontend React UI.</p>"
                 });
                 _context.SaveChanges();
             }
